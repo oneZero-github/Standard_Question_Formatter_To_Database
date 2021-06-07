82ed2bfa424865fcd441cd1a0d071e11d38931c5
NightStorm0 <itzgideonkane@gmail.com>
2021-06-07 19:42:53 +0100

    created project

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //Todo : Make a GUI of this shit later
+        //Todo : Make a GUI of this stuff later
+
+        /*
+            @Author : Gideon Kane M.
+            7th june 2021, This code is going to be the first
+            version of a database maker for the nova project which wll
+            soon be ported to all platforms. I will try to make this as generic as possible so i can port to other
+            implementations and frameworks
+         */
+        //for now main will serve as the GUI
+
+        setUpInterface();
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("Please input the path of the text file of questions   >");
+        String filePath = userInput.nextLine();
+
+    }
+
+    private static void setUpInterface() {
+
     }
 }
